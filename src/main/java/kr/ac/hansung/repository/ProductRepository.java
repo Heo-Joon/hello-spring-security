3a3d6049e48615ed410c3a9e4e416b9a0eeaf698
hsj031232 <hsj031232@gmail.com>
2026-06-01 08:54:40 +0900

    feat: 상품 페이징 Pageable 적용, 샘플 데이터 적용

--- src/main/java/kr/ac/hansung/repository/ProductRepository.java
+++ src/main/java/kr/ac/hansung/repository/ProductRepository.java
@@ -2,7 +2,16 @@ package kr.ac.hansung.repository;
 
 import kr.ac.hansung.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     long countByStockEquals(int stock);
+
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword%") // ✅ 추가
+    Page<Product> findByNameContaining(@Param("keyword") String keyword, Pageable pageable); // ✅ 추가
 }
